2c5042569bd4b0213f95e730f5cb804f70a6b5cb
Ankit-11525 <Ankitpathak11525@gmail.com>
2023-05-18 09:50:36 +0530

    navbar

--- frontend/src/components/shared/Navigation/Navigation.jsx
+++ frontend/src/components/shared/Navigation/Navigation.jsx
@@ -1,9 +1,16 @@
 import React from 'react'
-
+import {Link} from 'react-router-dom'
+import styles from './Navigation.module.css'
 const Navigation = () => {
   return (
-    <div> this is our Navigation page</div>
+    <nav className={ `${styles.navbar} container`}>
+      <Link to="/">
+        <img src='/images/logo.png' alt='Logo'/>
+        <span> DevSpace</span>
+      </Link>
+    </nav>
   )
 }
-
+// css module work only on it, not on child component so we define internal css for img and logo name 
+// Link used over a href because it works on without reloading
 export default Navigation
--- frontend/src/pages/Home/Home.jsx
+++ frontend/src/pages/Home/Home.jsx
@@ -2,7 +2,7 @@ import React from 'react'
 
 const Home = () => {
   return (
-    <div>Hello , Welcome to Home Page</div>
+    <div>Hello , Welcome to home page</div>
   )
 }
 
