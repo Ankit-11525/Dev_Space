6dbd55ef0c278274d12b1dd4e8f09546b9af582d
Aniket Kumar <aniketkumar@Anikets-MacBook-Air.local>
2023-05-20 22:11:47 +0530

    fix some issues

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -4,12 +4,17 @@ import Home from './pages/Home/Home.jsx';
 import Register from './pages/Register/Register';
 import Navigation from './components/shared/Navigation/Navigation'
 import Login from './pages/Login/Login';
+import Authenticate from './pages/authenticate/Authenticate';
+
+
 function App() {
   return <>
   <Router>
     <Navigation/>
     <Routes>
       <Route path="/" element={<Home />} />
+      <Route path="/authenticate" element={<Authenticate />} />
+      
       <Route path="/register" element={<Register />} />
       <Route path="/login" element={<Login />} />
     </Routes>
