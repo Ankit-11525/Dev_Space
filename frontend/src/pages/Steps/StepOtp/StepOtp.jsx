30f434efd79a3040b88b4b0345351936c0a2b096
Aniket Kumar <aniketkumar@Anikets-MacBook-Air.local>
2023-05-21 21:09:37 +0530

    Guest area front-end complete

--- frontend/src/pages/Steps/StepOtp/StepOtp.jsx
+++ frontend/src/pages/Steps/StepOtp/StepOtp.jsx
@@ -1,12 +1,32 @@
-import React from 'react'
+import React, { useState } from "react";
+import Card from "../../../components/shared/Card/Card";
+import Button from "../../../components/shared/Button/Button";
+import TextInput from "../../../components/shared/TextInput/TextInput";
+import styles from "./StepOtp.module.css";
 
-const StepOtp = ({onNext}) => {
+const StepOtp = ({ onNext }) => {
+  const [otp, setOtp] = useState("");
+
+  const next = () => {};
+
   return (
     <>
-      <div>Otp component</div>
-      <button onClick={onNext}>Next</button>
+    <div className={styles.cardWrapper}>
+    <Card title="Enter the code we just texted you" icon="lock-emoji">
+      <TextInput value={otp} onChange={(e) => setOtp(e.target.value)} />
+      <div>
+        <div className={styles.actionButtonWrap}>
+          <Button onClick={next} text="Next" />
+        </div>
+        <p className={styles.bottomParagraph}>
+          By entering your number, you're agreeing to our Terms of Service and
+          Privacy Policy. Thanks!
+        </p>
+      </div>
+    </Card>
+    </div>
     </>
-  )
-}
+  );
+};
 
-export default StepOtp
+export default StepOtp;
