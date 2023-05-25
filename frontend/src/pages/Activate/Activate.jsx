873d46d52d8e4c055bcfd36b63b0ae3ef607c7a8
vineet-0 <vineet351016@gmail.com>
2023-05-26 00:53:38 +0530

    fix issues

--- frontend/src/pages/Activate/Activate.jsx
+++ frontend/src/pages/Activate/Activate.jsx
@@ -1,23 +1,8 @@
-import React, {useState} from 'react';
-import StepName from '../Steps/StepName/StepName'
-import StepAvatar from '../Steps/StepAvatar/StepAvatar';
-
-const steps = {
-  1: StepName,
-  2: StepAvatar
-}
+import React from 'react'
 
 const Activate = () => {
-  const [step, setStep] = useState(1);
-  const Step = steps[step];
-
-  function onNext() {
-    setStep(step+1);
-  }
   return (
-    <div className='cardWrapper'>
-      <Step> onNext = {onNext}</Step>
-    </div>
+    <div>Activate Component</div>
   )
 }
 
