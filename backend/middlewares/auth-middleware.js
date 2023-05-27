7e93483b745dace773406795da7efad0fd88a575
vineet-0 <vineet351016@gmail.com>
2023-05-27 22:08:22 +0530

    room Create

---
+++ backend/middlewares/auth-middleware.js
@@ -0,0 +1,20 @@
+const AccessToken = require('twilio/lib/jwt/AccessToken');
+const tokenService = require('../services/token-service');
+
+module.exports = async function (req, res, next) {
+    try {
+        console.log(AccessToken);
+        const { accessToken } = req.cookies;
+        if (!accessToken) {
+            throw new Error();
+        }
+        const userData = await tokenService.verifyAccessToken(accessToken);
+        if (!userData) {
+            throw new Error();
+        }
+        req.user = userData;
+        next();
+    } catch (err) {
+        res.status(401).json({ message: 'Invalid token' });
+    }
+};
--- backend/server.js
+++ backend/server.js
@@ -1,23 +1,27 @@
 require('dotenv').config();
 const express = require('express');
-const router = require('./routes');
-const DbConnect = require('./database');
-const cors = require('cors');
-
-const corsOptions = {
-    origin: ['http://localhost:3000'],
-
-}
-
 const app = express();
-app.use(cors(corsOptions));
+const DbConnect = require('./database');
+const router = require('./routes');
+const cors = require('cors');
+const cookieParser = require('cookie-parser');
+
+app.use(cookieParser());
+
+const corsOption = {
+    credentials: true,
+    origin: ['http://localhost:3000'],
+};
+app.use(cors(corsOption));
+app.use('/storage', express.static('storage'));
+
 const PORT = process.env.PORT || 5500;
-DbConnect(); 
-app.use(express.json());
+DbConnect();
+app.use(express.json({ limit: '8mb' }));
 app.use(router);
 
-app.get('/', (req, res) =>{
-    res.send('Hello from express JS');
-})
+app.get('/', (req, res) => {
+    res.send('Hello from express Js');
+});
 
-app.listen(PORT, () => console.log(`Listening on Port ${PORT}`));
+app.listen(PORT, () => console.log(`Listening on port ${PORT}`));
