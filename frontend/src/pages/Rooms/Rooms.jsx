57f00122aca6d01d12dacdf8c220f74632ea6877
Aniket Kumar <aniketkumar@Anikets-MacBook-Air.local>
2023-05-30 21:49:57 +0530

    rooms page frontend

--- frontend/src/pages/Rooms/Rooms.jsx
+++ frontend/src/pages/Rooms/Rooms.jsx
@@ -1,9 +1,111 @@
-import React from 'react'
+import React from "react";
+import styles from "./Rooms.module.css";
+import RoomCard from "../../components/RoomCard/RoomCard";
+
+const rooms = [
+  {
+    id: 2,
+    topic: "Which framework is best for frontend ?",
+    speakers: [
+      {
+        id: 1,
+        name: "Ankit Pathak",
+        avatar: "/images/monkey-avatar.png",
+      },
+      {
+        id: 2,
+        name: "Cheems",
+        avatar: "/images/cheems.jpeg",
+      },
+    ],
+    totalPeople: 40,
+  },
+  {
+    id: 3,
+    topic: "Which framework is best for frontend ?",
+    speakers: [
+      {
+        id: 1,
+        name: "Ankit Pathak",
+        avatar: "/images/monkey-avatar.png",
+      },
+      {
+        id: 2,
+        name: "Cheems",
+        avatar: "/images/cheems.jpeg",
+      },
+    ],
+    totalPeople: 40,
+  },
+  {
+    id: 4,
+    topic: "Which framework is best for frontend ?",
+    speakers: [
+      {
+        id: 1,
+        name: "Aniket Kumar",
+        avatar: "/images/monkey-avatar.png",
+      },
+      {
+        id: 2,
+        name: "Cheems",
+        avatar: "/images/cheems.jpeg",
+      },
+    ],
+    totalPeople: 40,
+  },
+  {
+    id: 5,
+    topic: "Which framework is best for frontend ?",
+    speakers: [
+      {
+        id: 1,
+        name: "Vineet",
+        avatar: "/images/monkey-avatar.png",
+      },
+      {
+        id: 2,
+        name: "Cheems",
+        avatar: "/images/cheems.jpeg",
+      },
+    ],
+    totalPeople: 40,
+  },
+];
 
 const Rooms = () => {
   return (
-    <div>Rooms Component</div>
-  )
-}
+    <>
+      <div className="container">
+        <div className={styles.roomsHeader}>
+          <div className={styles.left}>
+            <span className={styles.heading}>All voice rooms</span>
+            <div className={styles.searchBox}>
+              <img src="/images/search-icon.png" alt="search" />
+              <input type="text" className={styles.searchInput} />
+            </div>
+          </div>
+          <div className={styles.right}>
+            <button className={styles.startRoomButton}>
+              <img src="/images/add-room-icon.png" alt="add-room" />
+              <span>Start a room</span>
+            </button>
+          </div>
+        </div>
 
-export default Rooms
+        <div className={styles.roomList}>
+          {rooms.map((room) => (
+            <>
+              <RoomCard key={room.id} room={room} />
+              <RoomCard key={room.id} room={room} />
+              <RoomCard key={room.id} room={room} />
+              <RoomCard key={room.id} room={room} />
+            </>
+          ))}
+        </div>
+      </div>
+    </>
+  );
+};
+
+export default Rooms;
